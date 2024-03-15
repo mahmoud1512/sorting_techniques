5af481659f45424afa77bd325406f9475693edac
omarkhaled <omarKhaled1853>
2024-03-16 01:30:57 +0200

    adding more diffrent types of generation

--- GenerateArray.java
+++ GenerateArray.java
@@ -1,7 +1,17 @@
 package org.example;
+import java.util.Arrays;
 import java.util.Random;
 
 public class GenerateArray {
+    private int[] reverse(int[] array){
+        int size = array.length;
+        for (int i = 0; i <= array.length / 2; i++) {
+            int t = array[i];
+            array[i] = array[size - i - 1];
+            array[size - i - 1] = t;
+        }
+        return array;
+    }
     public int[] generateRandomArray(int size) {
         Random random = new Random();
         int[] array = new int[size];
@@ -10,13 +20,22 @@ public class GenerateArray {
         }
         return array;
     }
-
-    public int[] generateRandomIntPositiveArray(int length) {
-        int[] array = new int[length];
+    public int[] generateRandomPositiveArray(int size) {
+        int[] array = new int[size];
         Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            array[i] = random.nextInt(10001);
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(1000001);
         }
         return array;
     }
+
+    public int[] generateSortedArray(int[] array){
+        Arrays.sort(array);
+        return array;
+    }
+
+    public int[] generateSortedReversedArray(int[] array){
+        return reverse(generateSortedArray(array));
+    }
+
 }
